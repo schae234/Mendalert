6bce905a82c8291aff5fd2af103e5bd0c100edc5
Rob Schaefer <schae234@umn.edu>
2012-11-26 17:21:15 -0600

    README and source comment update

---
+++ README.md
@@ -0,0 +1,39 @@
+Mendalert
+=========
+
+overview
+--------
+A perl script which, when cronned, checks a public mendeley group and checks if there are have been
+any recently added papers. For some reason, the notification system for papers in mendeley is broken.
+
+required packages
+-----------------
+use MIME::Lite;
+use Mail::Sendmail;
+use LWP::Simple;
+use JSON;
+use pQuery;
+
+
+what do?
+--------
+
+First, the script loads some external data which is stored as JSON. The old ds is variable holds 
+the JSON from an API request.
+
+The emails variable holds an arrayref of email addresses which will be notified when a new paper has been
+added. A valid DS looks like :
+
+```perl
+$emails = ['hello@gmail.com','world@gmail.com'];
+```   
+
+The Keys variable holds two key numbers, the group number and a mendeley api consumer key (register for 
+one at mendeley.com). They are in a hash:
+
+```perl
+$keys = {'group_id' = '12344321','consumer_key'='74830217480732184903217489032174809321' };
+```
+
+The script uses a get request on the mendeley api to check how many papers are added to the group.
+If its bigger than the cached version, it sends an email. 
--- mendalert.pl
+++ mendalert.pl
@@ -39,14 +39,20 @@ sub encode_json_to_file{
     print $json_file encode_json($ds) or die $!;
 }
 
+# load a working directory, we're not sure where perl is being executed from
 my $wd = "/heap/lab_website/mendalert/";
 
+# load in previously loaded group info (this might need to be preloaded or the script wont execute at first
 my $old_ds = decode_json_from_file($wd."group_info.json") or die $!;
+# loads an email list, its just an array of emails
 my $emails = $DEBUG ? ['schae234@umn.edu'] : decode_json_from_file($wd."emails.json")     or die $!;
+# we need API keys from mendeley so we can grab stuff over the API
 my $keys = decode_json_from_file($wd."tokens.json") or die $!;
 
+# Do a get request and transform the json the perl data structures
 my $ds = decode_json(get("http://api.mendeley.com/oapi/documents/groups/$keys->{'group_id'}/docs/?consumer_key=".$keys->{'consumer_key'})) or die $!;
 
+# do we have more papers now than we did last time?
 if($old_ds->{"total_results"} < $ds->{'total_results'}){
     # There is a new Paper!
     # Fetch the group info HTML
@@ -58,11 +64,13 @@ if($old_ds->{"total_results"} < $ds->{'total_results'}){
         Subject => 'New Paper Added To Mendeley',
         Type    => 'multipart/mixed',
     );
-    # Fetch the feed
+    # Fetch the feed, its public. We might as well make it pretty
     my $html = pQuery("http://www.mendeley.com/groups/2346321")
                         ->find("#group-overview-feed-block")
                         ->toHtml(); 
+    # using regex to edit the html is naughty. but whatevs.
     $html =~ s/style=".*"/style=""/g;
+    # send an email with html
     $msg->attach(
         Type => 'text/html',
         Data => '
